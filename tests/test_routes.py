25718c528e77bd40deb301e238a6690cdc8f53dd
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-11-01 12:36:24 +0300

    4 tests that have passed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,14 +1,39 @@
+# test_app.py
 import json
 import pytest
-from app import app # Import your Flask app
+from app import app
+
 
 class TestApp:
-     '''Moringa Daily Api  '''
-     def test_get_data_route(self):
-        '''has a resource at "/"'''
-        response = app.test_client().get('/')
-        assert response.status_code == 200  
-        data = json.loads(response.data.decode('utf-8'))  
-        assert data == {'message': 'Welcome to Moringa Daily'}
+    '''Moringa Daily Api Tests'''
+    def test_get_data_route(self):
+       response = app.test_client().get('/')
+       assert response.status_code == 200
+       data = json.loads(response.data.decode('utf-8'))
+       assert data ==  {'message' : 'Welcome to Moringa Daily'}
+       assert type (data)==dict
 
-     
+    def test_get_user_data_route(self):
+        response = app.test_client().get('/users')
+        assert response.status_code == 200
+        data = json.loads(response.data.decode('utf-8'))
+        assert type (data)==list
+        assert 'email' in data[0]  
+
+    def test_get_category_route(self):
+        response = app.test_client().get('/view-categories')    
+
+    def test_get_content_route(self):
+        response = app.test_client().get('/api/content')
+        assert response.status_code == 200
+        content_data = json.loads(response.data.decode('utf-8'))
+        assert type(content_data) == list
+        assert 'title' in content_data[0]
+
+
+    
+
+
+
+
+
